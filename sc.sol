a565682d02334cf1b29f8fa76458950e6a785a61
Schmidt <lena.schmidt@hpe.com>
2017-09-11 13:33:24 +0200

    variablen

--- sc.sol
+++ sc.sol
@@ -2,38 +2,69 @@ pragma solidity ^0.4.16;
 
 contract Raindatamonetizer {
     
-    uint requests;
-    uint payAmount; 
+    uint amountOfSegments;
+    uint remainingSegments;
+    uint pricePerSegment; 
     uint deadline; 
-    uint geolocationRadius;
+    int latitude;
+    int longitude;
+    uint radius;
+    uint funds;
     mapping (uint => Datasegment) datasegments;
     address owner;
     
     struct Datasegment {
         address provider;
-        uint geolocation;
-        bool raindata; 
+        int latitude;
+        int longitude;
+        uint raindata; 
         uint timestamp;
     }
     
-    function raindatamonetizer () {
-        owner = msg.sender;
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
     }
     
-    function insertDataSegment(uint geo, bool rain, uint time) returns (uint datasegmentID) {
+    function Raindatamonetizer (int _latitude, int _longitude, uint _radius, uint _amountOfSegments, uint _pricePerSegment, uint _deadline) payable {
+        require(msg.value > _pricePerSegment * _amountOfSegments);
+        funds = msg.value;
+        owner = msg.sender;
+        pricePerSegment = _pricePerSegment;
+        amountOfSegments = _amountOfSegments;
+        latitude = _latitude;
+        longitude = _longitude;
+        radius = _radius;
+        deadline = _deadline;
+    }
+    
+    function getConditions () constant returns (uint _pricePerSegment, int _latitude, int _longitude, uint _radius, uint _deadline) {
+        _pricePerSegment = pricePerSegment;
+        _latitude = latitude;
+        _longitude = longitude;
+        _radius = radius;
+        _deadline = deadline;
+    }
+    
+    function insertDataSegment(int _latitude, int _longitude, uint _raindata, uint _timestamp) returns (uint datasegmentID) {
         datasegmentID++; 
-        datasegments[datasegmentID] = Datasegment(msg.sender, geo, rain, time);
+        datasegments[datasegmentID] = Datasegment(msg.sender, _latitude, _longitude, _raindata, _timestamp);
+    }
+    
+    
+    function getDataSegment (uint datasegmentID) constant returns (address provider, int latitude, int longitude, uint raindata, uint timestamp) {
+        provider = datasegments[datasegmentID].provider;
+        latitude = datasegments[datasegmentID].latitude;
+        longitude = datasegments[datasegmentID].longitude;
+        raindata = datasegments[datasegmentID].raindata;
+        timestamp = datasegments[datasegmentID].timestamp;
+    }
+    
+    function escrow () {
+        
     }
     
     function validateDataSegment(uint iD) returns (bool valid) {
         
     }
-    
-    function getDataSegment (uint datasegmentID) returns (address provider, uint geolocation, bool raindata, uint timestamp) {
-        provider = datasegments[datasegmentID].provider;
-        geolocation = datasegments[datasegmentID].geolocation;
-        raindata = datasegments[datasegmentID].raindata;
-        timestamp = datasegments[datasegmentID].timestamp;
-    }
-    
 }
